1d8dd23078798173cfbaf01b4ebd213da870c0dc
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2025-08-01 23:45:55 +0300

    Add method to convert User instance to dictionary format

--- BaseMind.Data/Models/User.py
+++ BaseMind.Data/Models/User.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass, field
+from typing import Dict, Any
+from datetime import datetime
+
 @dataclass
 class User:
     user_id: int
@@ -9,3 +13,17 @@ class User:
     join_date: datetime = field(default_factory=datetime.now)
     last_activity: datetime = field(default_factory=datetime.now)
     message_count: int = 0
+
+    @classmethod
+    def to_dictionary(self) -> Dict[str, Any]:
+        return {
+            'user_id': self.user_id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'username': self.username,
+            'is_bot': self.is_bot,
+            'language_code': self.language_code,
+            'join_date': self.join_date,
+            'last_activity': self.last_activity,
+            'message_count': self.message_count
+        }
